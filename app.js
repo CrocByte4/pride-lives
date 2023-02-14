1fb2f5c640650e0a15f4997f3e29930e55718d62
Joseph Price <josephprice747@gmail.com>
2023-02-14 12:33:08 +0000

    add survey link, add stats page

--- app.js
+++ app.js
@@ -2,7 +2,19 @@
 // when clicked comes up with prompt with details and asks if user is willing to participate
 // if yes, begins survey function
 // if no, returns to index page
-function 
+const fomButton = document.getElementById("fom-button");
+
+fomButton.addEventListener("click", fomClick);
+
+function fomClick() {
+  let surveyStart = confirm(
+    "Thanks for looking. We are hoping to get a scope on the demographics of out site and have a few questions to ask. Some of your answers will be used for graphical representations, others will affect the content we present to you. We understand that we are asking for sensitive information, and for your peace of mind we will not hold any important identifying information about you and you will be given the option to skip questions. Are you happy to continue?"
+  );
+  if (surveyStart === true) {
+    alert("Thank you, you'll now be redirected to our survey");
+    location.href = "survey.html";
+  }
+}
 
 // survey function
 // ask a variety of questions, can include yes/no, pick from list of cat, pick country, etc
